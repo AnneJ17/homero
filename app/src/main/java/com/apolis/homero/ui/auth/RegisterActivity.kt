d453c260d83924845602784de26b2ac3bd7bc8a9
Anne Joseph <ajoseph13@ggc.edu>
2020-12-14 08:23:50 -0500

    Implemented UI for register screen

---
+++ app/src/main/java/com/apolis/homero/ui/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.apolis.homero.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.apolis.homero.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
