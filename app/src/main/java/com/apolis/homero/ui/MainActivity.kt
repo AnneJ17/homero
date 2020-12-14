33e3c587f53c6b8c682b7728935b14182418e5f7
Anne Joseph <ajoseph13@ggc.edu>
2020-12-13 21:11:51 -0500

    Initial commit

---
+++ app/src/main/java/com/apolis/homero/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.apolis.homero.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.apolis.homero.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
