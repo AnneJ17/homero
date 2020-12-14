3f4071a8bc924cff49df0ebd17ccb9ba514f42cb
Anne Joseph <ajoseph13@ggc.edu>
2020-12-14 00:38:42 -0500

    implemented UI for login screen

---
+++ app/src/main/java/com/apolis/homero/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.apolis.homero.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.apolis.homero.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
