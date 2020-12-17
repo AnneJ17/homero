b9b144fdfcf9e8197296ecb740bc264fb19a961e
Anne Joseph <ajoseph13@ggc.edu>
2020-12-17 00:00:01 -0500

    created ui for property detail screen

---
+++ app/src/main/java/com/apolis/homero/helpers/CustomRadioButton.kt
@@ -0,0 +1,7 @@
+package com.apolis.homero.helpers
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatRadioButton
+
+class CustomRadioButton (context: Context, attributes: AttributeSet) : AppCompatRadioButton(context, attributes), PrefixLayout.RadioButton
--- app/src/main/java/com/apolis/homero/helpers/PrefixLayout.kt
+++ app/src/main/java/com/apolis/homero/helpers/PrefixLayout.kt
@@ -2,11 +2,11 @@ package com.apolis.homero.helpers
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import com.apolis.homero.R
 import kotlinx.android.synthetic.main.prefix_layout.view.*
 
-class PrefixLayout(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+class PrefixLayout(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
     private var title: String = ""
         set(value) {
             field = value
@@ -14,7 +14,7 @@ class PrefixLayout(context: Context, attrs: AttributeSet) : LinearLayout(context
         }
 
     init {
-        orientation = VERTICAL
+//        orientation = VERTICAL
         inflate(context, R.layout.prefix_layout, this)
 
         //R.styleable has to be the same name as it is in attrs.xml
@@ -29,9 +29,7 @@ class PrefixLayout(context: Context, attrs: AttributeSet) : LinearLayout(context
     /** Important:
      * These interfaces act as keys, you need to make sure your CustomViews extend them accordingly.
      * This will make it easier to customise these views when we add them to this Layout.
-     * You can see how we've used these keys in line 32 and 36.
      */
     interface Spinner
-
-
+    interface RadioButton
 }
